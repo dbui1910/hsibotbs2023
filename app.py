9f82a9ecaf932fa846363ae77a3eb742ee21cd2a
meisullum <meiisullum@gmail.com>
2023-09-22 02:39:33 -0500

    cards

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('form.html')
+    return render_template('generate.html')
 
 openai.api_key = "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"
 
